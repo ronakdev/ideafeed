9207fedd40920839d9528f4fa9cfa7a7d4158908
Ronak Shah <ronakshahultimate@gmail.com>
2017-06-24 00:45:16 -0700

    Bare Minimum Working

--- js/main.js
+++ js/main.js
@@ -1,17 +1,58 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAU6CNPefTTPzXjC79mDwLUeUikMsLgBDg",
+    authDomain: "ideafeed-4f92d.firebaseapp.com",
+    databaseURL: "https://ideafeed-4f92d.firebaseio.com",
+    projectId: "ideafeed-4f92d",
+    storageBucket: "",
+    messagingSenderId: "85638812859"
+  };
+  firebase.initializeApp(config);
+var currentUser = prompt("Enter a username")
+
 var currentFilters = ["Javascript", "iOS"]
-var posts = [{profile: {username: "i_am_not_a_condiment", image: "temp.png"}, content: "Uber for Waffles", upvotes: 3, comments: ["Wow Amazing"]}]
-var currentUser = " i_am_not_a_condiment"
+
 class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {posts: [/*{username: "i_am_not_a_condiment", content: "Uber for Waffles", upvotes: 3, comments: ["Wow Amazing"] ,tags: ["Javascript"]}*/]}
+        this.getPosts = this.getPosts.bind(this)
+        this.updatePosts = this.updatePosts.bind(this)
+        
+
+    }
+    updatePosts(newPosts) {
+        this.setState({posts: newPosts})
+    }
+    getPosts() {
+        return this.state.posts
+    }
+    componentDidMount() {
+      var that = this;
+        console.log('before')
+      firebase.database().ref("posts").on("child_added", (child) => {
+        var p = that.state.posts;
+        console.log(child.val());
+        p.push(child.val());
+        that.setState({
+          posts: p,
+        });
+      }, (error) => {
+        console.log("error");
+      });
+    }
+    
     render() {
         return (
             <div>
-                <PostWriter />
-                
+                <TextForm handler={this}/>
+                <br/>
                 <Filters />
                 {
-                    posts.map(function(p) {
+                    
+                    this.state.posts.reverse().map((p, index) => {
                         return (
-                            <Post profile={p.profile} content={p.content} upvotes={p.upvotes} comments={p.comments} />
+                            <Post username={p.username} content={p.content} upvotes={p.upvotes} comments={p.comments} key={index} tags ={p.tags} handler={this}  getPosts={this.getPosts} updatePosts={this.updatePosts} />
                         )
 
                     })
@@ -23,27 +64,50 @@ class App extends React.Component {
 }
 
 class Post extends React.Component {
+    constructor(props) {
+        super(props)
+        this.upvotePost = this.upvotePost.bind(this)
+    
+    }
+    
+    upvotePost() {
+//                    var post = this.props.handler.state.posts[this.props.key]
+//                    console.log(post)
+//                    this.props.handler.state.posts.splice(this.props.key, 1)
+        //post.upvotes = post.upvotes + 1
+        //pushToServer(post)
+    }
     render() {
         return (
             <div className="row">
             <div className="col s12">
             <div className="card blue-grey darken-1 post">
                         <div className="card-content white-text">
-                            <span className="card-title"><i className="fa fa-user-circle" aria-hidden="true"></i> {this.props.profile.username} </span>
+                            <span className="card-title"><i className="fa fa-user-circle" aria-hidden="true"></i> {this.props.username} </span>
                             <p>{this.props.content}</p>
                             
                         </div>
                         <div className="card-action">
+                            {
+                   this.props.tags.map(function(filter, index) {
+                       return (
+                        <div className="chip" key={index}>
+                            {filter} <a href="#"><i className="fa fa-times" aria-hidden="true"></i></a>
+                        </div>
+                       )
+                   })
+               }    
+                        </div>
+                        <div className="card-action">
                             
-                            <div className="row">
-                                <span className="image-caption">{this.props.upvotes} upvotes {this.props.comments.length} comments</span>
-                            </div>
+                                <span className="image-caption">{this.props.upvotes} upvotes</span>
+                            
                         </div>
                         <div className="card-action center">
                             
                             <div className="row">
                                 <div className="col s6">
-                                    <a href="#">Comment</a>
+                                    <a href="#" onClick={this.upvotePost()}>Upvote</a>
                                 </div>
                                 <div className="col s6">
                                     <a href="#">Message</a>
@@ -64,9 +128,9 @@ class Filters extends React.Component {
                 <div className="col s12">
             <span>Current Filters: </span>
                {
-                   currentFilters.map(function(filter) {
+                   currentFilters.map(function(filter, index) {
                        return (
-                        <div className="chip">
+                        <div className="chip" key={index}>
                             {filter} <a href="#"><i className="fa fa-times" aria-hidden="true"></i></a>
                         </div>
                        )
@@ -108,18 +172,79 @@ class Navbar extends React.Component {
     }
 }
 
+class TextForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleContentChange = this.handleContentChange.bind(this);
+    this.handleTagChange = this.handleTagChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleContentChange(event) {
+    this.setState({content: event.target.value});
+  }
+
+  handleTagChange(event) {
+    this.setState({tags: event.target.value});
+  }
+
+  handleSubmit(event) {
+      var t = this.state.tags.split(',')
+      var post = {profile: {username: currentUser, image: "temp.png"}, content: this.state.content, upvotes: 0, comments: [], tags: t}
+
+      //this.handler.state.posts.unshift(post)
+    pushToServer(post)
+    this.props.handler.forceUpdate()
+
+    //firebase
+    //pushToServer(this.state.header, this.state.content)
+
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+    <div className="row">
+                <div className="col s12">
+                    <div className="card blue">
+                    <div className="card-content white-text">
+      <form onSubmit={this.handleSubmit} >
+          <div className="row">
+                        <label for="tags-input">Tags</label>
+
+          <input id="tags-input" type="text" value={this.state.header} onChange={this.handleTagChange} className="validate" />
+          </div>
+            <br />
+          
+          <div className="row">
+            <label for="content-input">Post Content</label>
+            
+          <textarea id="content-input" className="materialize-textarea text-black" type="text" value={this.state.content} onChange={this.handleContentChange} />
+        <br />
+          </div>
+        
+        <button className="waves-effect waves-light btn" type="button" value="Submit" onClick={this.handleSubmit}>Post</button>      </form>
+                        </div></div></div></div>
+    );
+  }
+}
+
+
+
 class PostWriter extends React.Component {
     
     constructor(props) {
         super(props)
         
         this.state = ({
-            tags: "",
-            content: ""
+            value:''
         })
-        this.addPost = this.addPost.bind(this)
+        
         this.handleContentChange = this.handleContentChange.bind(this)
         this.handleTagChange = this.handleTagChange(this)
+        this.addPost = this.addPost.bind(this)
     }
     
     addPost() {
@@ -154,20 +279,19 @@ class PostWriter extends React.Component {
                               
                               <div className="row">
                                 <div className="input-field col s12">
-                                  <textarea id="textarea1" className="materialize-textarea" value={this.state.content} onChange={this.handleContentChange} placeholder="Post Content"></textarea>
+                                  <textarea type="text" placeholder="Post Content" id="textarea1" className="materialize-textarea" value={this.state.content} onChange={this.handleContentChange.bind(this)}></textarea>
                                   <label for="textarea1">Content</label>
                                 </div>
                               </div>
                                 <div className="row">
                                 <div className="input-field col s12">
-                                  <input id="first_name" type="text" className="validate" value={this.state.tags} onChange={this.handleTagChange} placeholder="tags"/>
+                                  <input type="text" placeholder="tags" id="first_name" type="text" className="validate" value={this.state.tags} onChange={this.handleTagChange.bind(this)}/>
                                   <label for="first_name">Tags (seperate by commas)</label>
                                 </div>
                               </div>
                               <div className="row">
                                 <div className="input-field col s12">
                                   <a className="waves-effect waves-light btn" type="submit" >Post</a>
-
                                 </div>
                               </div>
                               
@@ -185,7 +309,30 @@ class PostWriter extends React.Component {
     }
 }
 
+function pullFromServer() {
+    var db = firebase.database();
+    var children = []; // get all of the posts from firebase and return them
+    db.ref("posts").once("child_added").then((child)=> {
+      children.push(child);
+    }).catch((error) => {
+      console.log(error.message);
+    });
+    return children;
+}
 
+function pushToServer(post) {
+    var db = firebase.database();
+
+    db.ref("posts").push().set({
+      content: post.content,
+      username: post.profile.username,
+      upvotes: post.upvotes,
+      comments: post.comments,
+      tags: post.tags
+    });
+    
+}
+//var posts = [{profile: {username: "i_am_not_a_condiment", image: "temp.png"}, content: "Uber for Waffles", upvotes: 3, comments: ["Wow Amazing"] ,tags: ["Javascript"]}]
 
 ReactDOM.render(<App/>, document.getElementById("main"))
 ReactDOM.render(<Navbar/>, document.getElementById("navigation"))
